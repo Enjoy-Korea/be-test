ff23b6e4dc4f21a61f90dd95907cf0467311bbcc
TaeHyeongKwon <1004kth1313@naver.com>
2023-04-20 06:10:57 +0900

    feat/typeorm을 이용한 DB연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+import { config } from 'dotenv';
+config();
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { House } from './entities/house.entity';
+import { Reservation } from './entities/reservation.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, House, Reservation],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
