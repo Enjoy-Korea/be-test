9ba8c3d39f6c349db7a00ad87295dc7576f904f1
TaeHyeongKwon <1004kth1313@naver.com>
2023-04-20 17:31:15 +0900

    feat/숙소 리스트 유효성 검사 추가

--- src/houses/houses.controller.ts
+++ src/houses/houses.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { HousesService } from './houses.service';
 import { FindAllHouseDto } from './dto/findall-house.dto';
 
@@ -7,6 +15,7 @@ export class HousesController {
   constructor(private readonly housesService: HousesService) {}
 
   @HttpCode(HttpStatus.OK)
+  @UsePipes(ValidationPipe)
   @Get('/list')
   findHouseList(@Query() findAllHouseDto: FindAllHouseDto) {
     return this.housesService.findHouseList(findAllHouseDto);
