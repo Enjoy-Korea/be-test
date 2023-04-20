e23d440cca3e16c4d885eb3a20177596b8154bb0
TaeHyeongKwon <1004kth1313@naver.com>
2023-04-20 17:30:30 +0900

    fix/ findall-house.dto 유효성 검사 수정

--- src/houses/dto/findall-house.dto.ts
+++ src/houses/dto/findall-house.dto.ts
@@ -1,14 +1,13 @@
-import { IsInt, IsString } from 'class-validator';
-
-export class FindAllHouseDto {
-  @IsInt()
-  page: number;
-
-  @IsString()
-  sort: ListSort;
-}
+import { IsEnum, IsNumberString } from 'class-validator';
 
 export enum ListSort {
   ASC = 'ASC',
   DESC = 'DESC',
+}
+export class FindAllHouseDto {
+  @IsNumberString()
+  page: string;
+
+  @IsEnum(ListSort)
+  sort: ListSort;
 }
